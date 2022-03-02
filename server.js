716947e6142e657ebd63e7dd8f415193020c847c
Fadima-doumbia <doumbia.fari@gmail.com>
2022-03-02 11:38:32 +0100

    heroku ajout du port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const port = process.env.port || 80;
+const port = process.env.PORT || 80;
 
 server.use(middlewares)
 server.use(router)
